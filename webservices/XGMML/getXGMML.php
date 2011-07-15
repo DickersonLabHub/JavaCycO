132da2bda8482473c03c9622c925133599196bf9
jlv <jlv@101e3190-ac79-410d-8174-9e528cef8c67>
2011-07-15 18:54:10 +0000

    update to use tht server

--- webservices/XGMML/getXGMML.php
+++ webservices/XGMML/getXGMML.php
@@ -1,27 +1,55 @@
 <?php
+$listorgs = isset($_GET["listorgs"]) ? strtoupper($_GET["listorgs"]) : false;
+$listpathways = isset($_GET["listpathways"]) ? strtoupper($_GET["listpathways"]) : false;
 $org = isset($_GET["org"]) ? strtoupper($_GET["org"]) : false;
 $pathway = isset($_GET["pathway"]) ? strtoupper($_GET["pathway"]) : false;
 if(isset($_GET["clean"])) $clean = $_GET["clean"];
 else $clean = "";
 
-if(!$org or !$pathway or strstr($org,";") or strstr($pathway,";"))
+if($listorgs)
+{
+	$filename = "orgs.txt";
+}
+elseif($listpathways)
+{
+	if($org)
+	{
+		$filename = "$org:pathways.txt";
+		if(!file_exists($filename) or $clean)
+		{
+			$cmd = ("grep $org pathways.txt > $filename");
+			//echo "$cmd\n";
+			exec($cmd);
+		}
+	}
+	else
+	{
+		$filename = "pathways.txt";
+	}
+}
+elseif((!$org and !$pathway) or strstr($org,";") or strstr($pathway,";"))
 {
 	include("index.php");
+	exit;
 }
-else
+elseif($org and $pathway)
 {
 
 	$filename = "$org:$pathway.xgmml";
 	if(!file_exists($filename) or $clean)
 	{
-		$cmd = ("java -jar NetworkExporter.jar vitis.student.iastate.edu 4444 $org $pathway xgmml biocyc-ecoli_at_vv_probeset_map.txt probeset > $filename");
+		$cmd = ("java -jar NetworkExporter.jar tht.vrac.iastate.edu 4444 $org $pathway xgmml biocyc-ecoli_at_vv_probeset_map.txt probeset > $filename");
 		//echo "$cmd\n";
 		exec($cmd);
 	}
-	header ("Content-Type: text/xml");
-	header("Content-Disposition: attachment; filename=$filename");
-	include($filename);
+
 }
+
+//header ("Content-Type: text/xml");
+//header("Content-Disposition: attachment; filename=$filename");
+//include($filename);
+header("Location: http://vitis.student.iastate.edu/XGMML/$filename");
+
 ?>
 
 
