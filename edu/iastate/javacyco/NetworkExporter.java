1ffda0e11b57621b35d710f6c1ca9c3a418a3d89
jlv <jlv@101e3190-ac79-410d-8174-9e528cef8c67>
2011-07-28 21:52:46 +0000

    added support for all pathways to NetworkExporter to be used to build pathwayflow inputs

--- edu/iastate/javacyco/NetworkExporter.java
+++ edu/iastate/javacyco/NetworkExporter.java
@@ -21,7 +21,7 @@ public class NetworkExporter {
 	public static void main(String[] args) {
 		if(args.length<4)
 		{
-			System.out.println("Usage: NetworkExporter SERVER PORT ORGANISM PATHWAY [gml|xgmml] [MAP_FILE [MAPPED_ATTRIBUTE_NAME]]");
+			System.out.println("Usage: NetworkExporter.jar SERVER PORT ORGANISM [PATHWAY|all] [gml|xgmml] [MAP_FILE [MAPPED_ATTRIBUTE_NAME]]");
 			System.exit(0);
 		}
 		String server = args[0];
@@ -42,27 +42,43 @@ public class NetworkExporter {
 		connection.selectOrganism(org);
 		try
 		{
-			Pathway pwy = (Pathway)Pathway.load(connection, pathway);
-			Network net = pwy.getNetwork();
-			if(mapFilename != null)
+			if(pathway.equals("all"))
 			{
-				BufferedReader mapFileReader = new BufferedReader(new FileReader(mapFilename));
-				String line = null;
-				HashMap<String,String> map = new HashMap<String,String>();
-				while((line=mapFileReader.readLine()) != null)
-				{
-					String[] lineParts = line.split("\t");
-					map.put(lineParts[0],lineParts[1]);
-				}
-				net.addMappedAttribute(mappedAttName, map);
-			}
-			if(format.equals("gml"))
-			{
-				net.writeGML(System.out,true,true,true,true,false,true);
+				Network net = connection.getNetwork();
+				net.writeGML(new PrintStream(new FileOutputStream(connection.getOrganismID()+".gml")),true,true,true,false,true,true);
+				//net.writeGML(new PrintStream(new FileOutputStream(connection.getOrganismID()+"_basic_unweighted_undirected.gml")),false,false,false,true,true,true);
+				//net.writeGML(new PrintStream(new FileOutputStream(connection.getOrganismID()+"_basic_weighted_undirected.gml")),false,true,false,true,true,true);
+				net.writeGML(new PrintStream(new FileOutputStream(connection.getOrganismID()+"_basic_unweighted_directed.gml")),false,false,true,true,true,true);
+				//net.writeGML(new PrintStream(new FileOutputStream(connection.getOrganismID()+"_basic_weighted_directed.gml")),false,true,true,true,true,true);
+				net.printGeneAltIDs();
+				net.printSynonyms();
+				connection.writeReactionNeighbors(new PrintStream(new FileOutputStream(connection.getOrganismID()+"_reaction_ORGS.tab")), "***PATHWAYS\t");
+				//connection.writeReactions(new PrintStream(new FileOutputStream(connection.getOrganismID()+"_reactions")), "***PATHWAYS\t");
 			}
 			else
 			{
-				net.writeXGMML(System.out,true,true,true,true,false,true);
+				Pathway pwy = (Pathway)Pathway.load(connection, pathway);
+				Network net = pwy.getNetwork();
+				if(mapFilename != null)
+				{
+					BufferedReader mapFileReader = new BufferedReader(new FileReader(mapFilename));
+					String line = null;
+					HashMap<String,String> map = new HashMap<String,String>();
+					while((line=mapFileReader.readLine()) != null)
+					{
+						String[] lineParts = line.split("\t");
+						map.put(lineParts[0],lineParts[1]);
+					}
+					net.addMappedAttribute(mappedAttName, map);
+				}
+				if(format.equals("gml"))
+				{
+					net.writeGML(System.out,true,true,true,true,false,true);
+				}
+				else
+				{
+					net.writeXGMML(System.out,true,true,true,true,false,true);
+				}
 			}
 		}
 		catch(Exception ex)
