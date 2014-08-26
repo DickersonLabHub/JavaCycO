3d8f774ed89c0b02ec688f8256700f3182b22025
Jesse Walsh <jrwalsh@iastate.edu>
2014-08-26 15:32:41 -0500

    Converted SVN project to Git project.  JavaCycO was originally developed as a module of the PathwayAccess project.  Here it is removed from the PathwayAccess project (which is no longer under development) so that development of JavaCycO is free to continue.

--- webservices/XGMML/refreshCache.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#You can run this to recreate the cache.  Will first delete the existing files, the call on NetworkExporter to recreate them.  This is useful if you 
-#anticipate that the underlying data has changed since the last time the files were created, but don't want to call each pathway individually to have its
-#xgmml file recreated.
-
-ORG=ARA
-rm -f $ORG*.xgmml
-java -jar NetworkExporter.jar tht.vrac.iastate.edu 4444 $ORG refreshCache xgmml biocyc-ecoli_at_vv_probeset_map.txt probeset
